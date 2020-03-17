d7fd3eabd52cd11927b1e845a3de7693bddcdd49
KK <kaihg1028@gmail.com>
2020-03-18 00:39:54 +0800

    ZigZagConversion and test

--- src/main/java/com/kk/leetcode/ZigZagConversion.java
+++ src/main/java/com/kk/leetcode/ZigZagConversion.java
@@ -3,21 +3,40 @@ package com.kk.leetcode;
 public class ZigZagConversion {
 
     public String convert(String s, int numRows) {
+        if (numRows==1){
+            return s;
+        }
+
         StringBuilder[] sbAry = new StringBuilder[numRows];
         for (int i = 0; i < numRows; i++) {
             sbAry[i] = new StringBuilder();
         }
 
+        int mod = 2 * (numRows - 1);
+        StringBuilder sb;
         for (int i = 0; i < s.length(); i++) {
-            int row = i % (numRows+1);
-            StringBuilder sb = sbAry[row];
+            int row_mod = i % mod;
+            int row = row_mod % numRows;
+
+            if (row_mod >= numRows){
+                sb = sbAry[mod - row_mod ];
+            }else{
+                sb = sbAry[row];
+            }
+
+//            if (row_mod != 0 && row == 0){
+//                sb = sbAry[numRows-2];
+//            } else{
+//                sb = sbAry[row];
+//            }
+
 
             sb.append(s.charAt(i));
         }
 
         StringBuilder ttlSb = new StringBuilder();
-        for (StringBuilder sb : sbAry) {
-            ttlSb.append(sb);
+        for (StringBuilder row_sb : sbAry) {
+            ttlSb.append(row_sb);
         }
 
         return ttlSb.toString();
--- src/test/java/com/kk/leetcode/ZigZagConversionTest.java
+++ src/test/java/com/kk/leetcode/ZigZagConversionTest.java
@@ -1,16 +1,37 @@
 package com.kk.leetcode;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ZigZagConversionTest {
 
+
     @org.junit.Test
     public void convert() {
 
         ZigZagConversion zzc = new ZigZagConversion();
-        String result = zzc.convert("PAYPALISHIRING", 3);
+
+        String result = zzc.convert("0123456", 3);
+        Assert.assertEquals("0413526", result);
+
+        result = zzc.convert("PAYPALISHIRING", 3);
         Assert.assertEquals("PAHNAPLSIIGYIR", result);
+
+        result = zzc.convert("0123456789", 4);
+        Assert.assertEquals("0615724839", result);
+
+        result = zzc.convert("PAYPALISHIRING", 5);
+        Assert.assertEquals("PHASIYIRPLIGAN", result);
+
+    }
+
+    @Test
+    public void testOne(){
+        ZigZagConversion zzc = new ZigZagConversion();
+
+        String result = zzc.convert("A", 1);
+        Assert.assertEquals("A", result);
     }
 }
